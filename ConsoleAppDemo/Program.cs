005d92532ca148036501383b7a6b4ba24deb6b93
unknown <MeltUser01@qut.edu.au>
2023-05-08 15:51:07 +1000

    added solution to workshop 9

---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleAppDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input file");
+            string path = Console.ReadLine();
+
+            DisplayFileContent(path);
+
+            DisplayMenu();
+        }
+
+        static void DisplayMenu()
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine(@"Select an option:
+1. Add unit
+2. Remove unit
+3. Read a new file
+4. Exit program");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        // Replace with your logic
+                        Console.WriteLine("Adding a unit");
+                        break;
+                    case "2":
+                        // Replace with your logic
+                        Console.WriteLine("Removing a unit");
+                        break;
+                    case "4":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+
+        static void DisplayFileContent(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FloydAlgorithm/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydsAlgorithm
+{
+    class Program
+    {
+
+        // Number of vertices in the graph
+        static int V = 5;
+
+        /* Define Infinite as a large enough value. This value will be used
+          for vertices not connected to each other */
+        static int INF = int.MaxValue / 2;
+
+
+        static void Main(string[] args)
+        {
+            /* Let us create the following weighted graph
+                   10
+              (0)------->(3)
+               |         /|\
+             5 |          |
+               |          | 1
+              \|/         |
+              (1)------->(2)
+                   3           */
+            int[,] graph = { {INF,   8,  INF, 9, 4},
+                        {INF, INF,   1, INF, INF},
+                        {INF, 2, INF,   3, INF},
+                        {INF, INF, 2, INF, 7},
+                        {INF, INF, 1, INF, INF}
+                      };
+
+            // Print the solution
+            FloydsAlgorithm(graph);
+            Console.ReadKey();
+        }
+
+        // A function to print the solution matrix
+        //  void printSolution(int dist[][V]);
+
+        // Solves the all-pairs shortest path problem using Floyd Warshall algorithm
+       static void FloydsAlgorithm(int[,] graph)
+        {
+            /* dist[,] will be the output matrix that will finally have the shortest
+              distances between every pair of vertices */
+
+            /* Initialize the solution matrix same as input graph matrix. Or
+               we can say the initial values of shortest distances are based
+               on shortest paths considering no intermediate vertex. */
+
+            /* Add all vertices one by one to the set of intermediate vertices.
+              ---> Before start of a iteration, we have shortest distances between all
+              pairs of vertices such that the shortest distances consider only the
+              vertices in set {0, 1, 2, .. k-1} as intermediate vertices.
+              ----> After the end of a iteration, vertex no. k is added to the set of
+              intermediate vertices and the set becomes {0, 1, 2, .. k} */
+            int[,] dist = graph;
+            // Number of nodes n
+            int n = dist.GetLength(0);
+
+            // Use k as the intermediate
+            for (int k = 0; k < n; k++)
+            {
+                // For each cell [i,j] in the current (meaning, from i to j)
+                // dist, is it better to go from i to j via k?
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        int attempt = dist[i, k] + dist[k, j];
+                        if (attempt < dist[i,j])
+                        {
+                            Console.WriteLine("i -> j: {0}", dist[i, j]);
+                            Console.WriteLine("i -> k: {0}", dist[i, k]);
+                            Console.WriteLine("k -> j: {0}", dist[k, j]);
+                            Console.WriteLine("i -> k -> j: {0}\n", attempt);
+                        }
+                        dist[i, j] = Math.Min(dist[i, j], attempt);
+                    }
+                }
+
+                Console.WriteLine("At k = {0}", k);
+                printSolution(dist);
+            }
+
+            // Print the shortest distance matrix
+            Console.WriteLine("Final solution:");
+            printSolution(dist);
+        }
+        /* A utility function to print solution */
+      static  void printSolution(int[,] dist)
+        {
+            Console.Write("Following matrix shows the shortest distances\n" +
+                    " between every pair of vertices \n");
+            Console.Write("     ");
+            for (int m = 0; m < V; m++)
+            {
+                Console.Write("        " + m);
+            }
+            Console.Write("\r\n");
+            Console.WriteLine("=======================================================");
+            for (int i = 0; i < V; i++)
+            {
+                Console.Write("    " + i);
+                for (int j = 0; j < V; j++)
+                {
+
+                    if (dist[i, j] == INF)
+                    {
+                        Console.Write("        " + "INF");
+                    }
+                    else
+                    {
+                        Console.Write("        " + dist[i, j]);
+                    }
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+}
