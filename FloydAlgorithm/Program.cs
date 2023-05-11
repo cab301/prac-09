7af84930396858662630ea62baf4b8a015c9dfb7
unknown <MeltUser01@qut.edu.au>
2023-05-11 18:47:23 +1000

    Added solution to workshop 9

---
+++ FloydAlgorithm/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydsAlgorithm
+{
+    class Program
+    {
+
+        // Number of vertices in the graph
+        static int V = 5;
+
+        /* Define Infinite as a large enough value. This value will be used
+          for vertices not connected to each other */
+        static int INF = 99999;
+
+
+        static void Main(string[] args)
+        {
+            /* Let us create the following weighted graph
+                   10
+              (0)------->(3)
+               |         /|\
+             5 |          |
+               |          | 1
+              \|/         |
+              (1)------->(2)
+                   3           */
+            //int[,] graph = { 
+            //            {INF,   8,  INF, 9, 4},
+            //            {INF, INF,   1, INF, INF},
+            //            {INF, 2, INF,   3, INF},
+            //            {INF, INF, 2, INF, 7},
+            //            {INF, INF, 1, INF, INF}
+            //          };
+
+            //int[,] graph =
+            //{
+            //    { INF, 20, INF, 60, INF, INF, 90, INF },
+            //    { INF, INF, INF, INF, INF, 10, INF, INF },
+            //    { INF, INF, INF, 10, INF, 10, INF, 20 },
+            //    { INF, INF, 10, INF, INF, INF, 20, INF },
+            //    { INF, 50, INF, INF, INF, INF, 30, INF },
+            //    { INF, INF, 10, 40, INF, INF, INF, INF },
+            //    { 20, INF, INF, INF, INF, INF, INF, INF },
+            //    { INF, INF, INF, INF, INF, INF, INF, INF },
+            //};
+
+            int[,] graph = Utilities.GetMatrixFromFile("../../../matrix.txt");
+            V = graph.GetLength(0);
+
+            // Print the solution
+            FloydsAlgorithm(graph);
+            Console.ReadKey();
+        }
+
+        // A function to print the solution matrix
+        //  void printSolution(int dist[][V]);
+
+        // Solves the all-pairs shortest path problem using Floyd Warshall algorithm
+       static void FloydsAlgorithm(int[,] graph)
+        {
+            /* dist[,] will be the output matrix that will finally have the shortest
+              distances between every pair of vertices */
+
+            /* Initialize the solution matrix same as input graph matrix. Or
+               we can say the initial values of shortest distances are based
+               on shortest paths considering no intermediate vertex. */
+
+            /* Add all vertices one by one to the set of intermediate vertices.
+              ---> Before start of a iteration, we have shortest distances between all
+              pairs of vertices such that the shortest distances consider only the
+              vertices in set {0, 1, 2, .. k-1} as intermediate vertices.
+              ----> After the end of a iteration, vertex no. k is added to the set of
+              intermediate vertices and the set becomes {0, 1, 2, .. k} */
+            int[,] dist = graph;
+
+            // Floyd's algorithm
+
+            // Intermediate node
+            for (int k = 0; k < graph.GetLength(0); k++)
+            {
+                // For any i,j: Is it better to go from i -> k then k -> j
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    for (int j = 0; j < graph.GetLength(0); j++)
+                    {
+                        // Better === Shorter, so use Min
+                        // dist[i, j]: current best distance from i to j
+                        // dist[i, k] + dist[k, j]: distance from i to j, if go via k
+                        dist[i, j] = Math.Min(dist[i, j], dist[i, k] + dist[k, j]);
+                    }
+                }
+            }
+
+            // Print the shortest distance matrix
+            printSolution(dist);
+        }
+        /* A utility function to print solution */
+      static  void printSolution(int[,] dist)
+        {
+            Console.Write("Following matrix shows the shortest distances\n" +
+                    " between every pair of vertices \n");
+            Console.Write("     ");
+            for (int m = 0; m < V; m++)
+            {
+                Console.Write("        " + m);
+            }
+            Console.Write("\r\n");
+            Console.WriteLine("=======================================================");
+            for (int i = 0; i < V; i++)
+            {
+                Console.Write("    " + i);
+                for (int j = 0; j < V; j++)
+                {
+
+                    if (dist[i, j] == INF)
+                    {
+                        Console.Write("        " + "INF");
+                    }
+                    else
+                    {
+                        Console.Write("        " + dist[i, j]);
+                    }
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+}
---
+++ FloydAlgorithm/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydsAlgorithm
+{
+    internal static class Utilities
+    {
+        static int INF = 99999;
+
+        public static int[,] GetMatrixFromFile(string path)
+        {
+            string matrixAsStr = GetTextFromPath(path);
+            return StringToMatrix(matrixAsStr);
+        }
+
+        public static string GetTextFromPath(string path)
+        {
+            // Check if the file exist. If not, return ""
+            if (File.Exists(path))
+            {
+                string result = "";
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line.Length > 0)
+                        {
+                            result += line + "\n";
+                        }
+                    }
+                }
+                return result;
+            }
+            return "";
+        }
+
+        public static int[,] StringToMatrix(string str)
+        {
+            string[] lines = str.Split("\n");
+            int columnCount = lines[0].Split("\t").Length;
+            int rowCount = lines.Length;
+            // Remove last row if empty
+            if (lines[rowCount - 1].Length == 0) rowCount--;
+
+            int[,] result = new int[rowCount, columnCount];
+            // Do the parsing
+            for (int i = 0; i < rowCount; i++)
+            {
+                string[] row = lines[i].Split('\t');
+                for (int j = 0; j < rowCount; j++)
+                {
+                    if (row[j] == "∞")
+                    {
+                        result[i, j] = INF;
+                    } else
+                    {
+                        result[i, j] = int.Parse(row[j]);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
